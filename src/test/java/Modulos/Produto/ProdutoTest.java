8b8a3d23bffc2d1bccbf4a64eeac2394cda764c3
Emmanuel Jesus Suarez Hermoso <suarezunesr@gmail.com>
2022-04-15 12:47:16 -0300

    inicializando o repositorio salvando arquivos

---
+++ src/test/java/Modulos/Produto/ProdutoTest.java
@@ -0,0 +1,65 @@
+package Modulos.Produto;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+@DisplayName("Testes de API Rest do modulo de Producto")
+public class ProdutoTest {
+    @Test
+    @DisplayName("Validar os limites proibidos do valor do produto")
+    public void testValidarLimitesProibidosValorProduto() {
+     // Configurando os dados da API rest da lojinha
+        baseURI = "http://165.227.93.41";
+       // port = 8080
+        basePath = "/lojinha";
+
+    //  Obter o token do usuario admin
+        String token = given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"usuarioLogin\": \"admin\",\n" +
+                        "  \"usuarioSenha\": \"admin\"\n" +
+                        "}")
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                .path("data.token");
+
+
+
+   //   Tentar inserir um produto com o valor  0.00 e validar a mensagem de error foi representada
+   //   Status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"celular\",\n" +
+                        "  \"produtoValor\": 0.00,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"branco\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"cargador\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+        .when()
+                .post("/v2/produtos")
+        .then()
+                .assertThat()
+                    .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                        .statusCode(422);
+    }
+}
